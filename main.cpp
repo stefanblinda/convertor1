e8323d7d6c746d3fad615eeb8810f9b03866506b
stefanblinda <stefan9725@yahoo.com>
2017-01-05 16:31:46 +0200

    ultimaversiune

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-double celsius,fahrenheit,cm,m,km,cm2,m2,km2,cm3,m3,km3,sec,mi,h,kh;
+double cm,m,km,cm2,m2,km2,cm3,m3,km3,sec,mi,h,kh,celsius,fahrenheit,mg,g,kg,j,cal,wh,bar,psi,mg3,g3,kg3,lk;
 //lungime//
 double convertCmM(){cm = cm / 100;}
 
@@ -51,12 +51,56 @@ double convertKmM3(){km3 = km3 * 1000000000;}
     double convertHmin(){h = h * 60;}
  //viteza//
 double convertKhMlh(){kh = kh / 1.609344;}
+
 double convertKhMs() {kh = kh * 0.27777777777778;}
 //temperatura//
-double convertF(){celsius = ((fahrenheit - 32) * 5) / 9;}
+    double convertFc(){fahrenheit = ((fahrenheit - 32) * 5) / 9;}
 
-double convertC(){fahrenheit = (((celsius * 9) / 5) + 32);}
+    double convertCf(){celsius = (((celsius * 9) / 5) + 32);}
+//masa//
+double convertMgG(){mg = mg / 1000;}
 
+double convertMgKg(){mg = mg / 1000000;}
+
+double convertGmg(){g = g * 1000;}
+
+double convertGkg(){g = g / 1000;}
+
+double convertKgMg(){kg = kg * 1000000;}
+
+double convertKgG(){kg = kg * 1000;}
+//energie//
+    double convertJcal(){j = j / 4.1858;}
+
+    double convertJwh(){j = j / 3600;}
+
+    double convertCalJ(){cal = cal * 4.1858;}
+
+    double convertCalWs(){cal = cal / 859.8452278589854;}
+
+    double convertWhJ(){wh = wh * 3600 ;}
+
+    double convertWhCal(){wh = wh * 859.8452278589854;}
+//presiune//
+double convertBarPsi(){bar = bar * 14.213698297557126;}
+
+double convertPsiBar(){psi = psi / 14.213698297557126;}
+//densitate//
+    double convertMgG3(){mg3 = mg3 / 1000;}
+
+    double convertMgKg3(){mg3 = mg3 / 1000000;}
+
+    double convertGmg3(){g3 = g3 * 1000;}
+
+    double convertGkg3(){g3 = g3 / 1000;}
+
+    double convertKgMg3(){kg3 = kg3 * 1000000;}
+
+    double convertKgG3(){kg3 = kg3 * 1000;}
+//consumcombustibil//
+double convertLkMlg(){lk = lk * 235.221;}
+
+double convertLkKl(){lk = lk * 100;}
 
 int main()
 {
@@ -79,7 +123,7 @@ Begin:	unsigned short choice;
 {
 case 1:
 
-B1:	    unsigned short c1;
+B1:	    unsigned short c1,choise2;
 
          cout << "Apasa '1' pentru a converti Centrimetri in Metri." << endl;
 	     cout << "Apasa '2' pentru a converti Centrimetri in Kilmetri." << endl;
@@ -169,6 +213,19 @@ B1:	    unsigned short c1;
 	      cout << "Ai ales gresit!" << endl;
 	      cout<<endl;
 	      goto B1;
+          }
+          cout << "Vrei sa continui?" << endl;
+          cout << "Apasa '1' daca DA." << endl;
+          cout << "Apasa '2' daca NU." << endl;
+          cin >> choise2;
+          switch(choise2)
+          {
+          case 1:
+            cout << endl;
+            goto Begin;
+            break;
+          case 2:
+            break;
           }
           break;
 
@@ -264,6 +321,19 @@ case 2:
 	      cout<<endl;
 	      goto B2;
           }
+          cout << "Vrei sa continui?" << endl;
+          cout << "Apasa '1' daca DA." << endl;
+          cout << "Apasa '2' daca NU." << endl;
+          cin >> choise2;
+          switch(choise2)
+          {
+          case 1:
+            cout << endl;
+            goto Begin;
+            break;
+          case 2:
+            break;
+          }
           break;
 
 case 3:
@@ -357,6 +427,19 @@ case 3:
 	      cout << "Ai ales gresit!" << endl;
 	      cout<<endl;
 	      goto B3;
+          }
+          cout << "Vrei sa continui?" << endl;
+          cout << "Apasa '1' daca DA." << endl;
+          cout << "Apasa '2' daca NU." << endl;
+          cin >> choise2;
+          switch(choise2)
+          {
+          case 1:
+            cout << endl;
+            goto Begin;
+            break;
+          case 2:
+            break;
           }
           break;
 
@@ -453,6 +536,19 @@ B4:	    unsigned short c4;
 	      cout<<endl;
 	      goto B4;
           }
+          cout << "Vrei sa continui?" << endl;
+          cout << "Apasa '1' daca DA." << endl;
+          cout << "Apasa '2' daca NU." << endl;
+          cin >> choise2;
+          switch(choise2)
+          {
+          case 1:
+            cout << endl;
+            goto Begin;
+            break;
+          case 2:
+            break;
+          }
           break;
 
 case 5:
@@ -494,6 +590,19 @@ B5:	    unsigned short c5;
 	      cout<<endl;
 	      goto B5;
           }
+          cout << "Vrei sa continui?" << endl;
+          cout << "Apasa '1' daca DA." << endl;
+          cout << "Apasa '2' daca NU." << endl;
+          cin >> choise2;
+          switch(choise2)
+          {
+          case 1:
+            cout << endl;
+            goto Begin;
+            break;
+          case 2:
+            break;
+          }
           break;
 
 case 6:
@@ -511,10 +620,10 @@ case 6:
 	      cout << "Scrie temperatura in Celsius.\n";
 
 	      cin >> celsius;
-	      convertC();
+	      convertCf();
 
 	      cout << "\n";
-	      cout << "Temperatura in Fahrenheit este " << fahrenheit << ".\n";
+	      cout << "Temperatura in Fahrenheit este " << celsius << ".\n";
 
 	      cout << endl;
 	      break;
@@ -524,10 +633,10 @@ case 6:
 	      cout << "Scrie temperatura in Fahrenheit.\n";
 
 	      cin >> fahrenheit;
-	      convertF();
+	      convertFc();
 
 	      cout << "\n";
-	      cout << "Temperatura in Celsius este " << celsius << ".\n";
+	      cout << "Temperatura in Celsius este " << fahrenheit << ".\n";
           cout << endl;
 	      break;
 
@@ -536,6 +645,450 @@ case 6:
 	      cout << "Ai ales gresit!" << endl;
 	      cout<<endl;
 	      goto B6;
+          }
+           cout << "Vrei sa continui?" << endl;
+          cout << "Apasa '1' daca DA." << endl;
+          cout << "Apasa '2' daca NU." << endl;
+          cin >> choise2;
+          switch(choise2)
+          {
+          case 1:
+            cout << endl;
+            goto Begin;
+            break;
+          case 2:
+            break;
+          }
+          break;
+
+case 7:
+
+B7:	    unsigned short c7;
+
+         cout << "Apasa '1' pentru a converti Miligrame in Grame." << endl;
+	     cout << "Apasa '2' pentru a converti Miligrame in Kilograme." << endl;
+	     cout << "Apasa '3' pentru a converti Grame in Miligrame." << endl;
+	     cout << "Apasa '4' pentru a converti Grame in Kilograme." << endl;
+	     cout << "Apasa '5' pentru a converti Kilograme in Miligrame." << endl;
+	     cout << "Apasa '6' pentru a converti Kilograme in Grame." << endl;
+         cin>> c7;
+       switch (c7)
+          {
+
+	      case 1:
+
+	      cout << "Scrie masa in Miligrame.\n";
+
+	      cin >> mg;
+	      convertMgG();
+
+	      cout << "\n";
+	      cout << "Masa in Grame este " << mg << ".\n";
+
+	      cout << endl;
+	      break;
+
+	      case 2:
+
+	      cout << "Scrie masa in Miligrame.\n";
+
+	      cin >> mg;
+	      convertMgKg();
+
+	      cout << "\n";
+	      cout << "Masa in Kilograme este " << mg << ".\n";
+          cout << endl;
+	      break;
+
+	      case 3:
+
+	      cout << "Scrie masa in Grame.\n";
+
+	      cin >> g;
+	      convertGmg();
+
+	      cout << "\n";
+	      cout << "Masa in Miligrame este " << g << ".\n";
+          cout << endl;
+	      break;
+
+          case 4:
+
+	      cout << "Scrie masa in Grame.\n";
+
+	      cin >> g;
+	      convertGkg();
+
+	      cout << "\n";
+	      cout << "Masa in Kilograme este " << g << ".\n";
+          cout << endl;
+	      break;
+
+          case 5:
+
+	      cout << "Scrie masa in Kilograme.\n";
+
+	      cin >> kg;
+	      convertKgMg();
+
+	      cout << "\n";
+	      cout << "Masa in Miligrame este " << kg << ".\n";
+          cout << endl;
+	      break;
+
+          case 6:
+
+	      cout << "Scrie masa in Kilograme.\n";
+
+	      cin >> kg;
+	      convertKgG();
+
+	      cout << "\n";
+	      cout << "Masa in Grame este " << kg << ".\n";
+          cout << endl;
+	      break;
+
+	      default:
+
+	      cout << "Ai ales gresit!" << endl;
+	      cout<<endl;
+	      goto B7;
+          }
+          cout << "Vrei sa continui?" << endl;
+          cout << "Apasa '1' daca DA." << endl;
+          cout << "Apasa '2' daca NU." << endl;
+          cin >> choise2;
+          switch(choise2)
+          {
+          case 1:
+            cout << endl;
+            goto Begin;
+            break;
+          case 2:
+            break;
+          }
+          break;
+
+case 8:
+
+B8:	    unsigned short c8;
+
+         cout << "Apasa '1' pentru a converti Jouli in Calorii." << endl;
+	     cout << "Apasa '2' pentru a converti Jouli in Watts/h." << endl;
+	     cout << "Apasa '3' pentru a converti Calorii in Jouli." << endl;
+	     cout << "Apasa '4' pentru a converti Calorii in Watts/h." << endl;
+         cout << "Apasa '5' pentru a converti Watts/h in Jouli." << endl;
+	     cout << "Apasa '6' pentru a converti Watts/h in Calorii." << endl;
+         cin>> c8;
+       switch (c8)
+          {
+
+          case 1:
+
+	      cout << "Scrie energia in Jouli.\n";
+
+	      cin >> j;
+	      convertJcal();
+
+	      cout << "\n";
+	      cout << "Energia in Calorii este " << j << ".\n";
+          cout << endl;
+	      break;
+
+          case 2:
+
+	      cout << "Scrie energia in Jouli.\n";
+
+	      cin >> j;
+	      convertJwh();
+
+	      cout << "\n";
+	      cout << "Energia in Watts/h este " << j << ".\n";
+          cout << endl;
+	      break;
+
+          case 3:
+
+	      cout << "Scrie energia in Calorii.\n";
+
+	      cin >> cal;
+	      convertCalJ();
+
+	      cout << "\n";
+	      cout << "Energia in Jouli este " << cal << ".\n";
+          cout << endl;
+	      break;
+
+          case 4:
+
+	      cout << "Scrie energia in Calorii.\n";
+
+	      cin >> cal;
+	      convertCalWs();
+
+	      cout << "\n";
+	      cout << "Energia in Watts/h este " << cal << ".\n";
+          cout << endl;
+	      break;
+
+          case 5:
+
+	      cout << "Scrie energia in Watts/h.\n";
+
+	      cin >> wh;
+	      convertWhJ();
+
+	      cout << "\n";
+	      cout << "Energia in Joulie este " << wh << ".\n";
+          cout << endl;
+	      break;
+
+          case 6:
+
+	      cout << "Scrie energia in Watts/h.\n";
+
+	      cin >> wh;
+	      convertWhCal();
+
+	      cout << "\n";
+	      cout << "Energia in Calorii este " << wh << ".\n";
+          cout << endl;
+	      break;
+
+	      default:
+
+	      cout << "Ai ales gresit!" << endl;
+	      cout<<endl;
+	      goto B8;
+          }
+          cout << "Vrei sa continui?" << endl;
+          cout << "Apasa '1' daca DA." << endl;
+          cout << "Apasa '2' daca NU." << endl;
+          cin >> choise2;
+          switch(choise2)
+          {
+          case 1:
+            cout << endl;
+            goto Begin;
+            break;
+          case 2:
+            break;
+          }
+          break;
+
+case 9:
+
+B9:	    unsigned short c9;
+
+         cout << "Apasa '1' pentru a converti Bar in Funte pe tol patrat." << endl;
+	     cout << "Apasa '2' pentru a converti Funte pe tol patrat in Bar." << endl;
+	     cin>> c9;
+       switch (c9)
+          {
+          case 1:
+
+	      cout << "Scrie presiunea in Bari.\n";
+
+	      cin >> bar;
+	      convertBarPsi();
+
+	      cout << "\n";
+	      cout << "Presiunea in Funti pe tol patrat este " << bar << ".\n";
+          cout << endl;
+	      break;
+
+          case 2:
+
+	      cout << "Scrie presiunea in Funti pe tol patrat.\n";
+
+	      cin >> psi;
+	      convertPsiBar();
+
+	      cout << "\n";
+	      cout << "Presiunea in Bari este " << psi << ".\n";
+          cout << endl;
+	      break;
+
+	      default:
+
+	      cout << "Ai ales gresit!" << endl;
+	      cout<<endl;
+	      goto B9;
+          }
+          cout << "Vrei sa continui?" << endl;
+          cout << "Apasa '1' daca DA." << endl;
+          cout << "Apasa '2' daca NU." << endl;
+          cin >> choise2;
+          switch(choise2)
+          {
+          case 1:
+            cout << endl;
+            goto Begin;
+            break;
+          case 2:
+            break;
+          }
+          break;
+
+case 10:
+
+B10:	    unsigned short c10;
+
+         cout << "Apasa '1' pentru a converti Miligrame/m3 in Grame/m3." << endl;
+	     cout << "Apasa '2' pentru a converti Miligrame/m3 in Kilograme/m3." << endl;
+	     cout << "Apasa '3' pentru a converti Grame/m3 in Miligrame/m3." << endl;
+	     cout << "Apasa '4' pentru a converti Grame/m3 in Kilograme/m3." << endl;
+	     cout << "Apasa '5' pentru a converti Kilograme/m3 in Miligrame/m3." << endl;
+	     cout << "Apasa '6' pentru a converti Kilograme/m3 in Grame/m3." << endl;
+         cin>> c10;
+       switch (c10)
+          {
+
+	      case 1:
+
+	      cout << "Scrie densitatea in Miligrame/m3.\n";
+
+	      cin >> mg3;
+	      convertMgG3();
+
+	      cout << "\n";
+	      cout << "Densitatea in Grame/m3 este " << mg3 << ".\n";
+
+	      cout << endl;
+	      break;
+
+	      case 2:
+
+	      cout << "Scrie densitatea in Miligrame/m3.\n";
+
+	      cin >> mg3;
+	      convertMgKg3();
+
+	      cout << "\n";
+	      cout << "Densitatea in Kilograme/m3 este " << mg3 << ".\n";
+          cout << endl;
+	      break;
+
+	      case 3:
+
+	      cout << "Scrie densitatea in Grame/m3.\n";
+
+	      cin >> g3;
+	      convertGmg3();
+
+	      cout << "\n";
+	      cout << "Densitatea in Miligrame/m3 este " << g3 << ".\n";
+          cout << endl;
+	      break;
+
+          case 4:
+
+	      cout << "Scrie densitatea in Grame/m3.\n";
+
+	      cin >> g3;
+	      convertGkg3();
+
+	      cout << "\n";
+	      cout << "Densitatea in Kilograme/m3 este " << g3 << ".\n";
+          cout << endl;
+	      break;
+
+          case 5:
+
+	      cout << "Scrie densitatea in Kilograme/m3.\n";
+
+	      cin >> kg3;
+	      convertKgMg3();
+
+	      cout << "\n";
+	      cout << "Densitatea in Miligrame/m3 este " << kg3 << ".\n";
+          cout << endl;
+	      break;
+
+          case 6:
+
+	      cout << "Scrie densitatea in Kilograme/m3.\n";
+
+	      cin >> kg3;
+	      convertKgG3();
+
+	      cout << "\n";
+	      cout << "Densitatea in Grame/m3 este " << kg3 << ".\n";
+          cout << endl;
+	      break;
+
+	      default:
+
+	      cout << "Ai ales gresit!" << endl;
+	      cout<<endl;
+	      goto B10;
+          }
+          cout << "Vrei sa continui?" << endl;
+          cout << "Apasa '1' daca DA." << endl;
+          cout << "Apasa '2' daca NU." << endl;
+          cin >> choise2;
+          switch(choise2)
+          {
+          case 1:
+            cout << endl;
+            goto Begin;
+            break;
+          case 2:
+            break;
+          }
+          break;
+
+case 11:
+
+B11:	    unsigned short c11;
+
+         cout << "Apasa '1' pentru a converti Litri/100km in Mile/galon." << endl;
+	     cout << "Apasa '2' pentru a converti Litri/100km in Kilometri/litru." << endl;
+	     cin>> c11;
+       switch (c11)
+          {
+          case 1:
+
+	      cout << "Scrie consumul de combustibil in Litri/100km.\n";
+
+	      cin >> lk;
+	      convertLkMlg();
+
+	      cout << "\n";
+	      cout << "Consumul de combustibil in Mile/galon este " << lk << ".\n";
+          cout << endl;
+	      break;
+
+          case 2:
+
+	      cout << "Scrie consumul de combustibil in Litri/100km.\n";
+
+	      cin >> lk;
+	      convertLkKl();
+
+	      cout << "\n";
+	      cout << "Consumul de combustibil in Kilometri/litru este " << lk << ".\n";
+          cout << endl;
+	      break;
+
+	      default:
+
+	      cout << "Ai ales gresit!" << endl;
+	      cout<<endl;
+	      goto B11;
+          }
+          cout << "Vrei sa continui?" << endl;
+          cout << "Apasa '1' daca DA." << endl;
+          cout << "Apasa '2' daca NU." << endl;
+          cin >> choise2;
+          switch(choise2)
+          {
+          case 1:
+            cout << endl;
+            goto Begin;
+            break;
+          case 2:
+            break;
           }
           break;
 
